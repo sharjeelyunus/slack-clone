b5087814469f7bf13fdb2b70911b5312ec1049a4
sharjeelyunus <sharjeel924@gmail.com>
2021-06-16 14:56:50 +0500

    📦 NEW: Cleanup + Firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2GMNbFaKrlHgiVwKdZU2_sLlLxUDOD3o",
+    authDomain: "slackbysharjeel.firebaseapp.com",
+    projectId: "slackbysharjeel",
+    storageBucket: "slackbysharjeel.appspot.com",
+    messagingSenderId: "1068773116746",
+    appId: "1:1068773116746:web:f102309bffdc544b544fb3"
+};

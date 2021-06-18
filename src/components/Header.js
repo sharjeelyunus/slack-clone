53a9acd5498e8eb5de237952b252e8d069173538
sharjeelyunus <sharjeel924@gmail.com>
2021-06-18 17:18:07 +0500

    👌 IMPROVE: Header Search

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
@@ -10,6 +12,13 @@ function Header() {
                 <HeaderAvatar />
                 <AccessTimeIcon />
             </HeaderLeft>
+            <HeaderSearch>
+                <SearchIcon />
+                <input placeholder="Search" />
+            </HeaderSearch>
+            <HeaderRight>
+                <HelpOutlineIcon />
+            </HeaderRight>
         </HeaderContainer>
     )
 }
@@ -45,5 +54,37 @@ const HeaderAvatar = styled(Avatar)`
     :hover {
         opacity: 0.8;
 
+    }
+`;
+
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    opacity: 1;
+    border-radius: 6px;
+    background-color: #421f44;
+    text-align: center;
+    display: flex;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid;
+
+    > input {
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30vw;
+        outline: 0;
+        color: white;
+    }
+`;
+
+const HeaderRight = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: flex-end;
+
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 20px;
     }
 `;

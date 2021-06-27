a23eabeee054436f48144a92cb628441d23ab9eb
sharjeelyunus <sharjeel924@gmail.com>
2021-06-28 00:02:00 +0500

    👌 IMPROVE: Add Channel & Channel Description

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,6 +7,20 @@ import ChatInput from './ChatInput';
 import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
 import { db } from '../firebase';
 import Message from './Message';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+        marginTop: '20px',
+        padding: '20px',
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
 
 function Chat() {
     const chatRef = useRef(null)
@@ -25,51 +39,98 @@ function Chat() {
         });
     }, [roomId, loading]);
 
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <DetailSidebar>
+                <h4><strong>#{roomDetails?.data().name}</strong></h4>
+                <p>{roomDetails?.data().description}</p>
+                <DetailBotton>
+                    <h5>Created By</h5>
+                    <img src={roomDetails?.data().userImage} alt="" />
+                    <p>{roomDetails?.data().user}</p>
+                    <span>{roomDetails?.data().timestamp?.toDate().toUTCString()}</span>
+                </DetailBotton>
+            </DetailSidebar>
+        </div>
+    );
+
     return (
+        <>
+            <ChatContainer>
+                {roomDetails && roomMessages && (
+                    <>
+                        <Header>
+                            <HeaderLeft>
+                                <h4><strong>#{roomDetails?.data().name}</strong></h4>
+                                <StarBorderOutlined />
+                            </HeaderLeft>
 
-        <ChatContainer>
-            {roomDetails && roomMessages && (
-                <>
-                    <Header>
-                        <HeaderLeft>
-                            <h4><strong>#{roomDetails?.data().name}</strong></h4>
-                            <StarBorderOutlined />
-                        </HeaderLeft>
+                            <HeaderRight>
+                                {['right'].map((anchor) => (
+                                    <React.Fragment key={anchor}>
+                                        <p onClick={toggleDrawer(anchor, true)}>
+                                            <InfoOutlined /> Details
+                                        </p>
+                                        <SwipeableDrawer
+                                            anchor={anchor}
+                                            open={state[anchor]}
+                                            onClose={toggleDrawer(anchor, false)}
+                                            onOpen={toggleDrawer(anchor, true)}
+                                        >
+                                            {list(anchor)}
+                                        </SwipeableDrawer>
+                                    </React.Fragment>
+                                ))}
+                            </HeaderRight>
+                        </Header>
 
-                        <HeaderRight>
-                            <p>
-                                <InfoOutlined /> Details
-                            </p>
-                        </HeaderRight>
-                    </Header>
+                        <ChatMessage>
+                            {roomMessages?.docs.map(doc => {
+                                const { message, timestamp, user, userImage } = doc.data();
 
-                    <ChatMessage>
-                        {roomMessages?.docs.map(doc => {
-                            const { message, timestamp, user, userImage } = doc.data();
+                                return (
+                                    <Message
+                                        key={doc.id}
+                                        message={message}
+                                        timestamp={timestamp}
+                                        user={user}
+                                        userImage={userImage}
+                                    />
+                                )
+                            })}
+                            <ChatBottom ref={chatRef} />
+                        </ChatMessage>
 
-                            return (
-                                <Message
-                                    key={doc.id}
-                                    message={message}
-                                    timestamp={timestamp}
-                                    user={user}
-                                    userImage={userImage}
-                                />
-                            )
-                        })}
-                        <ChatBottom ref={chatRef} />
-                    </ChatMessage>
+                        <ChatInput
+                            chatRef={chatRef}
+                            channelName={roomDetails?.data().name}
+                            channelId={roomId}
+                        />
+                    </>
+                )}
+            </ChatContainer>
 
-                    <ChatInput
-                        chatRef={chatRef}
-                        channelName={roomDetails?.data().name}
-                        channelId={roomId}
-                    />
-                </>
-            )}
-        </ChatContainer>
-
-
+        </>
 
     )
 }
@@ -112,11 +173,13 @@ const HeaderRight = styled.div`
         display: flex;
         align-items: center;
         font-size: 14px;
+        cursor: pointer;
     }
 
     > p > .MuiSvgIcon-root {
         margin-right: 5px !important;
         font-size: 16px;
+        cursor: pointer;
     }
 `;
 
@@ -124,4 +187,30 @@ const ChatMessage = styled.div``;
 
 const ChatBottom = styled.div`
     padding-bottom: 200px;
+`;
+
+const DetailSidebar = styled.div``;
+
+const DetailBotton = styled.div`
+  position: fixed;
+  bottom: 0;
+  text-align: center;
+  margin-bottom: 20px;
+
+  > h5 {
+      font-size: 16px;
+  }
+
+  > img {
+      margin-top: 5px;
+      border-radius: 50%;
+  }
+
+  > p {
+      font-weight: 600;
+  }
+
+  > span {
+      color: gray;
+  }
 `;
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { enterRoom } from '../features/appSlice';
-import { db } from '../firebase';
+import { auth, db } from '../firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import firebase from 'firebase';
+import { Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 function SidebarOption({ Icon, title, addChannelOption, id }) {
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [channelName, setChannelName] = useState('');
+    const [channelDescription, setChannelDescription] = useState('');
+    const [user] = useAuthState(auth);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     const dispatch = useDispatch();
 
-    const addChannel = () => {
-        const channelName = prompt("Please Enter the Channel Name");
+    const addChannel = (e) => {
+        e.preventDefault();
 
         if (channelName) {
             db.collection('rooms').add({
                 name: channelName,
+                description: channelDescription,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                user: user.displayName,
+                userImage: user.photoURL,
             })
         }
+
+        setChannelName('');
+        setChannelDescription('');
     };
 
     const selectChannel = () => {
@@ -26,18 +68,54 @@ function SidebarOption({ Icon, title, addChannelOption, id }) {
     };
 
     return (
-        <SidebarOptionContainer
-            onClick={addChannelOption ? addChannel : selectChannel}
-        >
-            {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
-            {Icon ? (
-                <h3>{title}</h3>
-            ) : (
-                <SidebarOptionChannel>
-                    <span>#</span> {title}
-                </SidebarOptionChannel>
-            )}
-        </SidebarOptionContainer>
+        <div>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <AddChannelContainer>
+                            <h2 id="transition-modal-title">Create Channel</h2>
+                            <input
+                                placeholder="Channel Name"
+                                onChange={e => setChannelName(e.target.value)}
+                                value={channelName}
+                            />
+                            <textarea
+                                cols="30"
+                                rows="10"
+                                placeholder="Channel Description"
+                                onChange={e => setChannelDescription(e.target.value)}
+                                value={channelDescription}
+                            ></textarea>
+                            <Button onClick={addChannel}>Create</Button>
+                        </AddChannelContainer>
+                    </div>
+                </Fade>
+            </Modal>
+
+            <SidebarOptionContainer
+                onClick={addChannelOption ? handleOpen : selectChannel}
+            >
+                {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+                {Icon ? (
+                    <h3>{title}</h3>
+                ) : (
+                    <SidebarOptionChannel>
+                        <span>#</span> {title}
+                    </SidebarOptionChannel>
+                )}
+            </SidebarOptionContainer>
+        </div>
     )
 }
 
@@ -67,4 +145,37 @@ const SidebarOptionContainer = styled.div`
 const SidebarOptionChannel = styled.h3`
     padding: 10px 0px;
     font-weight: 300;
+`;
+
+const AddChannelContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+
+    > h2 {
+        padding: 5px;
+    }
+
+    > input {
+        border: 1px solid gray;
+        border-radius:3px;
+        padding: 10px;
+        outline: none;
+        margin-bottom: 10px;
+    }
+
+    > textarea {
+        border: 1px solid gray;
+        border-radius:3px;
+        padding: 10px;
+        outline: none;
+        margin-bottom: 10px;
+
+    }
+
+    > button {
+      text-transform: inherit !important;
+      background-color: var(--slack-color) !important;
+      color: white;
+    }
 `;
